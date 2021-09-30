d50c2ea6cdeb516f99df300f282e72a7de30bdb9
Zedan Saheer <zed.saheer5@gmail.com>
2021-09-30 15:10:43 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Hello</h1>
+     <form className="form">
+     <label htmlFor="movieName">Movie Name</label>
+     <input type="text" name="movieName" />
+     <label htmlFor="review">Review</label>
+     <input type="text" name="review" />
+     <button>Submit</button>
+     </form>
     </div>
   );
 }
